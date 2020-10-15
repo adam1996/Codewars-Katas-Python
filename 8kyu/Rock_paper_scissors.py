f01a8e6f472c6c60c131fc98e59b8440bf4a2709
Adam Johnson <adam7819@googlemail.com>
2020-10-15 11:19:35 +0100

    Create Rock_paper_scissors.py
    added RPS solution

---
+++ 8kyu/Rock_paper_scissors.py
@@ -0,0 +1,19 @@
+"""
+Rock Paper Scissors! (8 kyu)
+
+https://www.codewars.com/kata/5672a98bdbdd995fad00000f
+
+Let's play! You have to return which player won! In case of a draw return Draw!.
+
+Examples:
+
+rps('scissors','paper') // Player 1 won!
+rps('scissors','rock') // Player 2 won!
+rps('paper','paper') // Draw!
+
+"""
+
+def rps(p1, p2):
+    hand = {'rock':0, 'paper':1, 'scissors':2}
+    results = ['Draw!', 'Player 1 won!', 'Player 2 won!']
+    return results[hand[p1] - hand[p2]]
