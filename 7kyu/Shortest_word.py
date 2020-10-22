1c3aef06bfb1f71b35c5b3deef1eeda9c985c2e8
Adam Johnson <adam7819@googlemail.com>
2020-10-22 10:56:42 +0100

    Create Shortest_word.py
    Using min function to find the len() of smallest word in a list of strings. Uses key=len param

---
+++ 7kyu/Shortest_word.py
@@ -0,0 +1,18 @@
+"""
+
+
+Shortest Word(7Kyu)
+https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/train/python
+
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+"""
+
+def find_short(s):
+    l = len(min(s.split(), key=len))
+    return l # l: shortest word length
+
+
+print(find_short("bitcoin take over the world maybe who knows perhaps"))
+
