539f4642a021df2f5094f39824bc01dee9d71dc3
Adam Johnson <adam7819@googlemail.com>
2020-10-26 16:04:03 +0000

    Create Remove_exclamation_marks.py

---
+++ 8kyu/Remove_exclamation_marks.py
@@ -0,0 +1,10 @@
+"""
+
+Remove exclamation marks (8Kyu)
+https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+"""
+
+def remove_exclamation_marks(s):
+    return s.replace("!", "")
