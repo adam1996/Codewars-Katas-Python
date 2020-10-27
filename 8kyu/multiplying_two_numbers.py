b8402935720d8ae098367e143fc2cd27bae092f0
Adam Johnson <adam7819@googlemail.com>
2020-10-27 12:57:17 +0000

    Create multiplying_two_numbers.py

---
+++ 8kyu/multiplying_two_numbers.py
@@ -0,0 +1,11 @@
+'''
+
+Function 3 - multiplying two numbers (8kyu)
+https://www.codewars.com/kata/523b66342d0c301ae400003b
+
+Implement a function which multiplies two numbers.
+
+'''
+
+def multiply(a, b):
+    return a * b
