05234297bcebca5e2df79c645570158225d86d8b
Adam Johnson <adam7819@googlemail.com>
2020-10-27 14:13:38 +0000

    Create Find_the_odd_int.py

---
+++ 6yu/Find_the_odd_int.py
@@ -0,0 +1,16 @@
+"""
+Find the odd int (6Kyu)
+https://www.codewars.com/kata/54da5a58ea159efa38000836
+
+Given an array of integers, find the one that appears an odd number of times.
+
+There will always be only one integer that appears an odd number of times.
+"""
+
+
+def find_it(seq):
+    for i in seq:
+        if seq.count(i) % 2 == 1: return i
+
+
+print(find_it([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]))
