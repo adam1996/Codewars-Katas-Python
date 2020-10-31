fc08e64d93bf8bfdbb0f35fd5005cabec44da789
Adam Johnson <adam7819@googlemail.com>
2020-10-31 11:36:23 +0000

    Update Sort_and_star.py

--- 8kyu/Sort_and_star.py
+++ 8kyu/Sort_and_star.py
@@ -8,11 +8,14 @@ You will be given a vector of strings. You must sort it alphabetically (case-sen
 The returned value must be a string, and have "***" between each of its letters.
 
 You should not remove or add elements from/to the array.
+
+def two_sort(array):
+    return '***'.join(min(array))
 '''
 
 def two_sort(array):
 
-    return "***".join([i for i in (sorted(array)[0])])
+    return '***'.join(sorted(array)[0])
 
 
 print(two_sort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]))
