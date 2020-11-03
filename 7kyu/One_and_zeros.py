075758eab8429931790d9f0f1af4d00c237e74f5
Adam Johnson <adam7819@googlemail.com>
2020-11-03 21:54:44 +0000

    Create One_and_zeros.py
    Use map(str, arr) to convert int list to str list

---
+++ 7kyu/One_and_zeros.py
@@ -0,0 +1,27 @@
+"""
+Ones and Zeros (7Kyu)
+https://www.codewars.com/kata/578553c3a1b8d5c40300037c
+
+Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+Examples:
+
+Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 0, 1] ==> 5
+Testing: [1, 0, 0, 1] ==> 9
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 1, 0] ==> 6
+Testing: [1, 1, 1, 1] ==> 15
+Testing: [1, 0, 1, 1] ==> 11
+
+def binary_array_to_number(arr):
+  return int("".join(map(str, arr)), 2)
+"""
+
+def binary_array_to_number(arr):
+    return int("".join(["{}".format(i) for i in arr]), 2)
+
+print(binary_array_to_number([0, 0, 0, 1]))
