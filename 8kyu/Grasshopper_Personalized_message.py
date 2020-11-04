83c9f7a63bce25f279c1bf54d53f5c9157501c74
Adam Johnson <adam7819@googlemail.com>
2020-11-04 19:15:19 +0000

    Create Grasshopper_Personalized_message.py

---
+++ 8kyu/Grasshopper_Personalized_message.py
@@ -0,0 +1,17 @@
+'''
+
+Grasshopper - Personalized Message (8kyu)
+https://www.codewars.com/kata/5772da22b89313a4d50012f7
+
+Personalized greeting
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	return
+name equals owner	'Hello boss'
+otherwise	'Hello guest'
+
+'''
+def greet(name, owner):
+    return "Hello boss" if name == owner else "Hello guest"
