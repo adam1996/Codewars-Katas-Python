26dcdaeb0b062f5ab3b79c3f6f3f87c09f3cb5e2
Adam Johnson <adam7819@googlemail.com>
2020-11-07 20:31:55 +0000

    Create five_without_numbers.py

---
+++ 8kyu/five_without_numbers.py
@@ -0,0 +1,14 @@
+'''
+5 without numbers !! (8kyu)
+https://www.codewars.com/kata/59441520102eaa25260000bf
+
+Write a function that always returns 5
+
+Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+Good luck :)
+
+
+'''
+def unusual_five():
+    return len("five!")
