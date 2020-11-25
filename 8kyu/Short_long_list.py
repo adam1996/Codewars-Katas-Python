d99b04b16cc272e6367d22fddc3bca0f448844cf
Adam Johnson <adam7819@googlemail.com>
2020-11-25 16:55:34 +0000

    Create Short_long_list.py

---
+++ 8kyu/Short_long_list.py
@@ -0,0 +1,24 @@
+"""
+Short Long Short (8Kyu)
+https://www.codewars.com/kata/50654ddff44f800200000007
+
+Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( length 0 ).
+
+For example:
+
+solution("1", "22") # returns "1221"
+solution("22", "1") # returns "1221"
+
+"""
+
+def solution(a, b):
+    print("Hello")
+    input_lst = [a, b]
+    input_lst.sort(key=len, reverse=False) 
+    return input_lst[0] + input_lst[1] + input_lst[0]
+
+    #get the len of a and b 
+
+
+print(solution("Adam", "Johnson"))
+print("Hello")
