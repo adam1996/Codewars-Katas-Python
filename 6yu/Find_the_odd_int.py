084c86108b68911611e9330ec99eae03e8915c95
Adam Johnson <adam7819@googlemail.com>
2020-11-25 16:55:32 +0000

    Update Find_the_odd_int.py

--- 6yu/Find_the_odd_int.py
+++ 6yu/Find_the_odd_int.py
@@ -11,6 +11,8 @@ There will always be only one integer that appears an odd number of times.
 def find_it(seq):
     for i in seq:
         if seq.count(i) % 2 == 1: return i
+        seq.clear
+        print
 
 
 print(find_it([20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5]))
