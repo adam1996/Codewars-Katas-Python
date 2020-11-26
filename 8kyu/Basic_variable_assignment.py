ab72de76b664deaef96ca0b1922a05a388124383
Adam Johnson <adam7819@googlemail.com>
2020-11-26 17:54:48 +0000

    Create Basic_variable_assignment.py

---
+++ 8kyu/Basic_variable_assignment.py
@@ -0,0 +1,15 @@
+"""
+
+Basic variable assignment (8Kyu)
+https://www.codewars.com/kata/50ee6b0bdeab583673000025
+
+This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+a == "code"
+b == "wa.rs"
+name == a + b
+"""
+
+a = "code"
+b = "wa.rs"
+name = a + b
