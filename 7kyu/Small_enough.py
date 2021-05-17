3c1e95930c7eb107e2a17d1b6bdd1d222cfd2c20
Adam Johnson <adam7819@googlemail.com>
2021-05-17 20:55:51 +0100

    create Small_enough.py

---
+++ 7kyu/Small_enough.py
@@ -0,0 +1,14 @@
+"""
+Small enough? - Beginner! (7Kyu)
+https://www.codewars.com/kata/57cc981a58da9e302a000214
+
+You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value.
+If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers.
+"""
+def small_enough(array, limit):
+    return not any([val > limit for val in array])
+
+print(small_enough([100] ,100))
+#(false, true, true, true )
