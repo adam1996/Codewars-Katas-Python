77f4b1630a64a6e692c267b51de7aedff8fb6e68
Adam Johnson <adam7819@googlemail.com>
2021-05-18 19:30:01 +0100

    Created 7kyu

--- 7kyu/Small_enough.py
+++ 7kyu/Small_enough.py
@@ -6,9 +6,14 @@ You will be given an array and a limit value. You must check that all values in 
 If they are, return true. Else, return false.
 
 You can assume all values in the array are numbers.
+
+def small_enough(array, limit):
+    return max(array)<=limit
+
 """
 def small_enough(array, limit):
     return not any([val > limit for val in array])
 
 print(small_enough([100] ,100))
 #(false, true, true, true )
+
